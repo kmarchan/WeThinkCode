2fbd45c2b1e0d217d46813497f43b093e52257ec
kmarchan <kmarchan@student.wethinkcode.co.za>
2018-05-22 13:18:08 +0200

    updated 22/05

--- in_progress/strnstr/main.c
+++ in_progress/strnstr/main.c
@@ -6,17 +6,24 @@
 /*   By: kmarchan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/22 09:46:07 by kmarchan          #+#    #+#             */
-/*   Updated: 2018/05/22 09:57:27 by kmarchan         ###   ########.fr       */
+/*   Updated: 2018/05/22 12:26:23 by kmarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 int	main()
 {
-	char str[] = "fluffy";
-	char find[] = "uf";
-	ft_putstr(ft_strnstr(str, find, 3));
-	ft_putstr(strnstr(str, find, 3));
+	char str[] = "A slightly longer string.";
+	char find[] = "lon";
+//	ft_putstr(ft_strnstr(str, find, 3));
+//	ft_putstr(strnstr(str, find, 17));
+	if (!(ft_strnstr(str, find, 3)))
+	{
+		printf("Good");
+	}
+//	printf("%s\n", ft_strnstr(str, find, 3));
+//	printf("%s", strnstr(str, find, 3));
 	return (0);
 }
