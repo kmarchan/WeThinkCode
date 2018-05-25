0892cc9c21845f328300334167c31400e81beacc
kmarchan <kmarchan@student.wethinkcode.co.za>
2018-05-25 14:42:29 +0200

    unfinished

--- in_progress/strlcat/ft_strlcat.c
+++ in_progress/strlcat/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: kmarchan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/24 13:37:20 by kmarchan          #+#    #+#             */
-/*   Updated: 2018/05/25 10:36:38 by kmarchan         ###   ########.fr       */
+/*   Updated: 2018/05/25 14:41:47 by kmarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,23 @@
 
 size_t	ft_strlcat(char *dest, const char *src, size_t dlen)
 {
-	size_t	i;
-	size_t	s;
+	size_t i;
+//	size_t s;
+//	int maxcpy;
 
-	s = 0;
-	i = dlen - 1;
-	while (src[s] != '\0' && s < dlen)
+//	maxcpy = dlen - ft_strlen(dest) - 1;
+//	s = 0;
+	i = (ft_strlen(dest) + ft_strlen(src));
+	if (dlen > 0 && ft_strlen(dest) < dlen)
 	{
-		dest[i++] = src[s++];
+		ft_strncat(dest, src, dlen - ft_strlen(dest) - 1);
+	//	while (src[s] != '\0' && i < dlen)
+	//	{
+	//		dest[i++] = src[s++];
+	//	}
+	//	dest[i] = '\0';
+		return (i);
 	}
-	if (dlen > 0 || i != dlen)
-		dest[i] = '\0';
-	return (i);
+	else 
+		return (0);
 }
--- in_progress/strlcat/main.c
+++ in_progress/strlcat/main.c
@@ -5,11 +5,12 @@
 int main(void)
 {
 	char s1[20] = "fluffy";
-	char s2[7] = "fluffi";
+	//char s2[7] = "";
 	char t1[20] = "fluffy";
-	char t2[7] = "fluffi";
-	printf("ft_%lu\n", ft_strlcat(s1, s2, 7));
-	printf("c. %lu\n", strlcat(t1, t2, 7));
+	//char t2[7] = "";
+	printf("ft_%lu\n", ft_strlcat(s1, "", 18));
+	printf("c. %lu\n", strlcat(t1, ""
+				, 18));
 	return (0);
 }
 
