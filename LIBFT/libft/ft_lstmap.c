36996bb6a8047d88b263b8e9f0ceed02e9275ae4
Kmarchan <kmarchan@student.wethinkcode.co.za>
2018-08-26 13:18:31 +0200

    retubmitted

--- LIBFT/libft/ft_lstmap.c
+++ LIBFT/libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: kmarchan <kmarchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/23 18:12:11 by kmarchan          #+#    #+#             */
-/*   Updated: 2018/08/24 18:25:24 by kmarchan         ###   ########.fr       */
+/*   Updated: 2018/08/26 12:07:37 by kmarchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,20 @@ t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 	t_list *new;
 	t_list *new1;
 
-	new = NULL;
+	if (!lst || !f)
+		return (NULL);
+	new = f(lst);
 	new1 = new;
 	lst = lst->next;
-	while (lst)
+	while (lst != NULL)
 	{
-		new = (t_list *)ft_memalloc(sizeof(lst->content_size));
-		if (!lst)
+		new->next = (t_list *)ft_memalloc(sizeof(lst->content_size));
+		new->next = f(lst);
+		if (!new)
 			return (NULL);
-		new = f(lst);
-		new->next = NULL;
 		lst = lst->next;
+		new = new->next;
+		new->next = NULL;
 	}
 	new = new1;
 	return (new);
